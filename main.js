d60656fbc1012c7710d645df1e0a69dd4c93bb6e
3Mar <73734731+OmAr490@users.noreply.github.com>
2024-06-02 15:12:15 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+
+const searchBox = document.getElementById("search-box") 
+const timeEle = document.getElementById("time")
+
+const date = new Date();
+let hours = date.getHours();
+let min = date.getMinutes();
+let mer = hours >= 12 ? "pm" : "am";
+
+hours = hours % 12;
+hours = hours ? hours : 12;
+
+timeEle.innerHTML = `${hours}:${min} ${mer}`;
+
+searchBox.addEventListener("keypress", ({ key }) => {
+    if(key == 'Enter') {
+        search();
+    }
+})
+
+function search() {
+    let url = 'https://www.google.com/search?q=' + searchBox.value;
+    console.log(url)
+    window.location.assign(url)
+}
+
+
